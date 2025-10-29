4d8422e59c09e19d68b825f57cad2cd40730c490
rocky000552-star <rocky000552@gmail.com>
2025-10-29 22:18:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+const serverExports = {
+  NextRequest: require('next/dist/server/web/spec-extension/request')
+    .NextRequest,
+  NextResponse: require('next/dist/server/web/spec-extension/response')
+    .NextResponse,
+  ImageResponse: require('next/dist/server/web/spec-extension/image-response')
+    .ImageResponse,
+  userAgentFromString: require('next/dist/server/web/spec-extension/user-agent')
+    .userAgentFromString,
+  userAgent: require('next/dist/server/web/spec-extension/user-agent')
+    .userAgent,
+}
+
+if (typeof URLPattern !== 'undefined') {
+  // eslint-disable-next-line no-undef
+  serverExports.URLPattern = URLPattern
+}
+
+// https://nodejs.org/api/esm.html#commonjs-namespaces
+// When importing CommonJS modules, the module.exports object is provided as the default export
+module.exports = serverExports
+
+// make import { xxx } from 'next/server' work
+exports.NextRequest = serverExports.NextRequest
+exports.NextResponse = serverExports.NextResponse
+exports.ImageResponse = serverExports.ImageResponse
+exports.userAgentFromString = serverExports.userAgentFromString
+exports.userAgent = serverExports.userAgent
+exports.URLPattern = serverExports.URLPattern
