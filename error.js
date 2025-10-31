8ed6f5090840d3c238338d8eb06f424637540577
rocky000552-star <rocky000552@gmail.com>
2025-10-31 16:51:26 +0530

    Add files via upload

--- error.js
+++ error.js
@@ -1,1 +1,4 @@
-module.exports = require('./dist/pages/_error')
+throw new Error(
+  "This module cannot be imported from a Server Component module. " +
+    "It should only be used from a Client Component."
+);
